c057cece4242e0c175a51d80e8034fc2385fa49d
Ivar Fernandez <ivarfermar7@gmail.com>
2021-11-11 13:28:48 +0100

    02- Commit desde clase

--- app.java
+++ app.java
@@ -2,7 +2,8 @@ public class HolaGente {
  
 	public static void main(String[] args) {		
 		System.out.println("Hola");
-		System.out.println("Esto es otro proyecto");		
+		System.out.println("Esto es otro proyecto");
+		System.out.println("Modificado desde clase");		
 	}
  
 }
